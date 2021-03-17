1f66ec36048ce48d3bfdbd2bd07fbd78a9f93980
Rafael Costa <rafael.rac.mg@gmail.com>
2021-03-17 06:52:50 -0300

    feat: book entity and create book dto

---
+++ BookStore.Domain/DTOs/CreateBookRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Domain.DTOs
+{
+    public class CreateBookRequestDTO
+    {
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Required]
+        [StringLength(80, MinimumLength = 4)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(80, MinimumLength = 4)]
+        public string Subtitle { get; set; }
+
+        [Required]
+        [StringLength(80, MinimumLength = 4)]
+        public string Subject { get; set; }
+    }
+}
--- BookStore.Domain/Entities/Book.cs
+++ BookStore.Domain/Entities/Book.cs
@@ -1,8 +1,17 @@
-﻿namespace BookStore.Domain.Entities
+﻿using System;
+
+namespace BookStore.Domain.Entities
 {
     public class Book
     {
+        public int Id { get; set; }
         public int AuthorId { get; set; }
         public Author Author { get; set; }
+        public string Guid { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Subject { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
