f67e70e0b5a3776a1c5efed55c5c44788e95b877
Rafael Costa <rafael.rac.mg@gmail.com>
2021-03-17 07:02:52 -0300

    feat: book controller

---
+++ BookStore.Application/UseCase/BookUseCase.cs
@@ -0,0 +1,38 @@
+﻿using BookStore.Domain.DTOs;
+using BookStore.Domain.Entities;
+using BookStore.Domain.Interfaces;
+using BookStore.Shared.Notifications;
+using BookStore.Shared.Utils;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.UseCase
+{
+    public class BookUseCase : IBookUseCase
+    {
+        public Task<Either<NotificationBase, Book>> CreateBook(CreateBookRequestDTO create)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Either<NotificationBase, bool>> DeleteAnBookById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Either<NotificationBase, IEnumerable<Author>>> GetAllBooks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Either<NotificationBase, Author>> GetAnBookById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Either<NotificationBase, Author>> UpdateAnBookById(int id, object update)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- BookStore.Infrastructure.IoC/IoCExtensions.cs
+++ BookStore.Infrastructure.IoC/IoCExtensions.cs
@@ -20,6 +20,7 @@ namespace BookStore.Infrastructure.IoC
             services.AddScoped<ISessionUseCase, SessionUseCase>();
             services.AddScoped<IUserUseCase, UserUseCase>();
             services.AddScoped<IAuthorUseCase, AuthorUseCase>();
+            services.AddScoped<IBookUseCase, BookUseCase>();
             return services;
         }
         public static IServiceCollection AddRepositories(this IServiceCollection services) 
